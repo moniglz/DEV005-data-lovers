73118462aecb84d45f59b5be4d3dbf779ad96a28
moniglz <monie.gop@gmail.com>
2023-03-10 13:44:03 -0600

    ADD document.createElement for containerCards on movies page & UPDATE style.css

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@
 import data from "./data/ghibli/ghibli.js";
 
 const dataGhibli = data.films;
-
 //console.log(dataGhibli);
-
+const containerCards = document.getElementById("cont-movies");
 dataGhibli.forEach(element => {
   console.log(element);
-  
+  const divCard = document.createElement("div");
+  const imgCard = document.createElement("img");
+  const spanCard = document.createElement("span");
+  divCard.classList = "card";
+  imgCard.src = `${element.poster}`;
+  spanCard.innerHTML = `${element.title}`;
+  divCard.appendChild(imgCard);
+  divCard.appendChild(spanCard);
+  containerCards.appendChild(divCard);
 });
+
