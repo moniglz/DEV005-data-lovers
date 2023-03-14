1da17546255e90209fe5c564d803648ab6c98b02
ifdotcom <ifervb@gmail.com>
2023-03-14 16:12:45 -0600

    UPDATE: createElement to innerHTML

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import { dataFilmsReverse, dataFilmsSort } from "./data.js";
 import data from "./data/ghibli/ghibli.js";
 
 const dataFilms = data.films;
-// createCards(dataFilms);
-
+createCards(dataFilms);
 
 // Obtener la opcion del select
 
@@ -13,8 +12,6 @@ filters.addEventListener("change", () => {
   const indexSelect = filters.selectedIndex;
   if (indexSelect === 1) {
     const dataOrdenada = dataFilmsSort(dataFilms);
-    // remover nodo
-    // deleteNodo();
     createCards(dataOrdenada);
   } else if (indexSelect === 2) {
     const dataReverse = dataFilmsReverse(dataFilms);
@@ -26,25 +23,18 @@ filters.addEventListener("change", () => {
 
 function createCards(dataSort) {
   const containerCards = document.getElementById("cont-movies");
+  containerCards.innerHTML = "";
   dataSort.forEach((element) => {
-    //console.log(element);
     const divCard = document.createElement("div");
-    const imgCard = document.createElement("img");
-    const spanCard = document.createElement("span");
     divCard.classList = "card";
-    imgCard.src = `${element.poster}`;
-    spanCard.innerHTML = `${element.title}`;
-    divCard.appendChild(imgCard);
-    divCard.appendChild(spanCard);
+    divCard.innerHTML = `
+    <img src="${element.poster}">
+    <span>${element.title}</span>
+   `;
     containerCards.appendChild(divCard);
   });
 }
 
-// function deleteNodo() {
-//   const containerCards = document.getElementById("cont-movies");
-//   const cards = document.querySelectorAll(".card");
 
-//   containerCards.removeChild(cards);
-// }
 /*const search = document.querySelectorAll("#search");
 search.addEventListener("keyup",)*/
